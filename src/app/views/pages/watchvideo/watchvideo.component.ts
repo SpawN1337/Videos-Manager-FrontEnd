4d2e73d5ccea5cd8c4cd5fd2e49e355f861bf8a9
MonTa <39422192+SpawN1337@users.noreply.github.com>
2023-03-28 04:19:45 +0100

    added error if there is no video

--- src/app/views/pages/watchvideo/watchvideo.component.ts
+++ src/app/views/pages/watchvideo/watchvideo.component.ts
@@ -1,5 +1,9 @@
 import { Component } from '@angular/core';
 import { ActivatedRoute, Router } from '@angular/router';
+import { environment } from '../../../../environments/environment';
+import { VideoService } from "../../../services/video.service";
+import { ToastrService } from 'ngx-toastr';
+
 @Component({
   selector: 'app-watchvideo',
   templateUrl: './watchvideo.component.html',
@@ -7,14 +11,26 @@ import { ActivatedRoute, Router } from '@angular/router';
 })
 export class WatchvideoComponent {
   id: any;
+  baseUrl = environment.baseUrl;
 
 
-
-  constructor(private activatetRoute: ActivatedRoute,
-    ) {
+  constructor(private activatetRoute: ActivatedRoute,private toasterService: ToastrService,private videoService: VideoService
+  ) {
   }
   ngOnInit(): void {
-  this.id = this.activatetRoute.snapshot.params['id'];
-}
-
+    this.id = this.activatetRoute.snapshot.params['id'];
+    this.getres()
+  }
+  getres(){
+    this.videoService.getVideo(this.id).subscribe((response: any) => {
+      console.log(response)
+    }
+    ,(error) => {
+      this.toasterService.error( error.error.message,'خطأ', {
+        timeOut: 9000,
+      });
+      console.log("Error",error.error.message);
+    }
+    );
+  }
 }
